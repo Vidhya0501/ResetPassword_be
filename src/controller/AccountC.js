3bf4698a1b65132ec2012e253a10d02d9673089e
Vidhya0501 <vidhyasuri97@gmail.com>
2023-10-22 11:44:51 +0530

    reset password updated

--- src/controller/AccountC.js
+++ src/controller/AccountC.js
@@ -1,12 +1,122 @@
 import AccountModel from "../models/AccountM.js"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import nodemailer from 'nodemailer'
 
 //register - post
-const register=(req,res)=>{
-    AccountModel.create(req.body)
-    .then(accounts=>res.json(accounts))
-    .catch(err=>res.json(err))
+const register=async(req,res)=>{
+    try {
+        let account= await  AccountModel.create(req.body)
+        if(account){
+            res.send(account)
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            message:"Internal Server Error",
+            error:error.message
+        })
+    }
+   
 }
 
+
+//login - post
+const login=async(req,res)=>{
+    try {
+        const {email,password}=req.body
+        let user = await AccountModel.findOne({email:email})
+        if(user){
+            if(user.password===password){
+                res.send("Login successful")
+            }
+            else{
+                res.send("Incorrect Password")
+            }
+        }
+        else{
+            res.json("Not yet registered")
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            message:"Internal Server Error",
+            error:error.message
+        })
+    }
+}
+
+const forgotPassword=async (req,res)=>{
+    try {
+        const {email}=req.body
+        let user = await AccountModel.findOne({email:email})
+        if(!user){
+            return res.send({Status:"User not exist"})
+        }
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRE})
+            
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: 'vidhyasuri97@gmail.com',
+                  pass: 'Vidhyaviji'
+                }
+              });
+              
+              var mailOptions = {
+                from: 'vidhyasuri97@gmail.com',
+                to: 'kmusvidhya@gmail.com',
+                subject: "Reset your password",
+                text: `http://localhost:5173/reset-password/${user._id}/${token}`
+              };
+              
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  return res.send({Status:"Success"})
+                }
+              });
+            
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            message:"Internal Server Error",
+            error:error.message
+        })
+    }
+}
+
+const resetPassword = async (req,res)=>{
+    try {
+        const {id,token}=req.params
+        const {password}=req.body
+    
+        await jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+            if(err){
+                return res.send({Status:"Error in token"})
+            }
+            else{
+                let hash= bcrypt.hash(password,process.env.SALT_ROUNDS)
+                if(hash){
+                    AccountModel.findByIdAndUpdate({_id:id},{password:hash})
+                    res.send({Status:"Success"})
+                }
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            message:"Internal Server Error",
+            error:error.message
+        })
+    }
+   
+}
 export default{
-    register
+    register,
+    login,
+    forgotPassword,
+    resetPassword
 }
--- src/routes/AccountR.js
+++ src/routes/AccountR.js
@@ -4,5 +4,8 @@ import AccountController from '../controller/AccountC.js'
 const router = express.Router()
 
 router.post('/register',AccountController.register)
+router.post('/login',AccountController.login)
+router.post('/forgot-password',AccountController.forgotPassword)
+router.post('/reset-password/:id/:token',AccountController.resetPassword)
 
 export default router
